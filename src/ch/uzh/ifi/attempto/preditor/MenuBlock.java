d2c25fcdc35f38aa8f0a4a8755e298cae001bfbc
Tobias Kuhn <kuhntobias@gmail.com>
2010-06-14 15:08:47 -0400

    Better implementation of asynchronous menu creation.

--- src/ch/uzh/ifi/attempto/preditor/MenuBlock.java
+++ src/ch/uzh/ifi/attempto/preditor/MenuBlock.java
@@ -17,6 +17,7 @@ package ch.uzh.ifi.attempto.preditor;
 import java.util.List;
 
 import nextapp.echo2.app.Alignment;
+import nextapp.echo2.app.ApplicationInstance;
 import nextapp.echo2.app.Button;
 import nextapp.echo2.app.Color;
 import nextapp.echo2.app.Column;
@@ -25,11 +26,10 @@ import nextapp.echo2.app.Extent;
 import nextapp.echo2.app.Font;
 import nextapp.echo2.app.Insets;
 import nextapp.echo2.app.SplitPane;
+import nextapp.echo2.app.TaskQueueHandle;
 import nextapp.echo2.app.event.ActionListener;
-import ch.uzh.ifi.attempto.echocomp.DelayedComponent;
 import ch.uzh.ifi.attempto.echocomp.Label;
 import ch.uzh.ifi.attempto.echocomp.Style;
-import ch.uzh.ifi.attempto.echocomp.VSpace;
 import ch.uzh.ifi.attempto.echocomp.WindowPane;
 
 /**
@@ -45,6 +45,15 @@ class MenuBlock extends SplitPane {
 	private ActionListener actionListener;
 	private Column menuColumn = new Column();
 	private Button label = new Button("...");
+
+	private int state = 0;
+	private List<MenuItem> items;
+	private int width;
+	private int progress;
+	private final ApplicationInstance app;
+	private final TaskQueueHandle taskQueue;
+	
+	private final int lazyEvalStep = 25;
 	
 	/**
 	 * Creates a new menu block.
@@ -54,6 +63,8 @@ class MenuBlock extends SplitPane {
 	 */
 	public MenuBlock(ActionListener actionListener, WindowPane parent) {
 		super(ORIENTATION_VERTICAL_TOP_BOTTOM, new Extent(16));
+		this.app = ApplicationInstance.getActive();
+		this.taskQueue = app.createTaskQueue();
 		this.actionListener = actionListener;
 		
 		label.setEnabled(false);
@@ -110,45 +121,73 @@ class MenuBlock extends SplitPane {
 	 * @param width The width of this menu block in pixels.
 	 * @param pageSize The vertical page size in number of items.
 	 */
-	public void setContent(final MenuBlockContent content, final int width, final int pageSize) {
+	public synchronized void setContent(final MenuBlockContent content, final int width, final int pageSize) {
+		state++;
 		label.setText(content.getName());
 		label.setWidth(new Extent(width - 3));
 		menuColumn.removeAll();
-		
-		final List<MenuItem> items = content.getItems();
+		this.width = width;
+
+		items = content.getItems();
+		progress = 0;
 		
 		if (items.size() > pageSize) {
-			for (int i=0; i < pageSize; i++) {
-				MenuItem m = items.get(i);
-				m.setWidth(new Extent(width - 24));
-				m.addActionListener(actionListener);
-				menuColumn.add(m);
-			}
-			
-			menuColumn.add(new DelayedComponent(new VSpace((items.size()-pageSize)*15)) {
-				
-				private static final long serialVersionUID = -5737622027016695873L;
-
-				public Component initComponent() {
-					Column c = new Column();
-					for (int i = pageSize; i < items.size() ; i++) {
-						MenuItem m = items.get(i);
-						m.setWidth(new Extent(width - 24));
-						m.addActionListener(actionListener);
-						c.add(m);
-					}
-					return c;
-				}
-				
-			});
-			
+			menuColumn.add(createNextMenuComponent());
+			calculateRest(state);
 		} else {
 			for (MenuItem m : items) {
 				menuColumn.add(m);
 				m.setWidth(new Extent(width - 7));
 				m.addActionListener(actionListener);
 			}
+			progress = items.size();
 		}
+	}
+	
+	private synchronized void calculateRest(final int state) {
+		if (progress >= items.size()) return;
+		if (this.state != state) return;
+
+		Thread thread = new Thread() {
+			public void run() {
+				final Component c = createNextMenuComponent();
+				
+				app.enqueueTask(
+					taskQueue,
+					new Runnable() {
+						public void run() {
+							addMenuComponent(c, state);
+						}
+					}
+				);
+			}
+		};
+		thread.start();
+	}
+	
+	private synchronized void addMenuComponent(Component c, int state) {
+		if (this.state != state) return;
+		menuColumn.add(c);
+		calculateRest(state);
+	}
+	
+	private synchronized Component createNextMenuComponent() {
+		Column c = new Column();
+		int endPos = progress + lazyEvalStep;
+		if (endPos > items.size()) endPos = items.size();
+		for (int i = progress ; i < endPos ; i++) {
+			MenuItem m = items.get(i);
+			m.setWidth(new Extent(width - 24));
+			m.addActionListener(actionListener);
+			c.add(m);
+		}
+		progress = endPos;
+		return c;
+	}
+	
+	public void setVisible(boolean visible) {
+		state++;
+		super.setVisible(visible);
 	}
 
 }
