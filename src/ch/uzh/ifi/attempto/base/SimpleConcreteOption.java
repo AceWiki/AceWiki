99e8c874d59c13a34783add0cc8a7bb7fdd16a92
Tobias Kuhn <kuhntobias@gmail.com>
2011-09-08 15:54:30 +0200

    Simple implementations for next token options

---
+++ src/ch/uzh/ifi/attempto/base/SimpleConcreteOption.java
@@ -0,0 +1,55 @@
+// This file is part of AceWiki.
+// Copyright 2008-2011, Tobias Kuhn.
+// 
+// AceWiki is free software: you can redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+// 
+// AceWiki is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along with AceWiki. If
+// not, see http://www.gnu.org/licenses/.
+
+package ch.uzh.ifi.attempto.base;
+
+/**
+ * This is a simple implementation of a concrete option.
+ * 
+ * @author Tobias Kuhn
+ */
+public class SimpleConcreteOption implements ConcreteOption {
+	
+	private String word;
+	private String categoryName;
+	
+	/**
+	 * Creates a new concrete option for the given word and category name.
+	 * 
+	 * @param word The word.
+	 * @param categoryName The category name.
+	 */
+	public SimpleConcreteOption(String word, String categoryName) {
+		this.word = word;
+		this.categoryName = categoryName;
+	}
+	
+	/**
+	 * Creates a new concrete option for the given word, without a category.
+	 * 
+	 * @param word The word.
+	 */
+	public SimpleConcreteOption(String word) {
+		this.word = word;
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+}
---
+++ src/ch/uzh/ifi/attempto/base/SimpleNextTokenOptions.java
@@ -0,0 +1,78 @@
+// This file is part of AceWiki.
+// Copyright 2008-2011, Tobias Kuhn.
+// 
+// AceWiki is free software: you can redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+// 
+// AceWiki is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along with AceWiki. If
+// not, see http://www.gnu.org/licenses/.
+
+package ch.uzh.ifi.attempto.base;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This is a simple implementation of NextTokenOptions.
+ * 
+ * @author Tobias Kuhn
+ */
+public class SimpleNextTokenOptions implements NextTokenOptions {
+
+	private Set<ConcreteOption> cOptions = new HashSet<ConcreteOption>();
+	private Set<AbstractOption> aOptions = new HashSet<AbstractOption>();
+	private Set<String> tokens = new HashSet<String>();
+	private Set<String> categories = new HashSet<String>();
+	
+	/**
+	 * Creates a new object for the given concrete and abstract options.
+	 * 
+	 * @param cOptions The concrete options.
+	 * @param aOptions The abstract options.
+	 */
+	public SimpleNextTokenOptions(Set<ConcreteOption> cOptions, Set<AbstractOption> aOptions) {
+		if (cOptions != null) {
+			this.cOptions = cOptions;
+			for (ConcreteOption o : cOptions) {
+				tokens.add(o.getWord());
+			}
+		}
+		if (aOptions != null) {
+			this.aOptions = aOptions;
+			for (AbstractOption o : aOptions) {
+				categories.add(o.getCategoryName());
+			}
+		}
+	}
+	
+	/**
+	 * Creates a new object for the given concrete, without any abstract options.
+	 * 
+	 * @param cOptions The concrete options.
+	 */
+	public SimpleNextTokenOptions(Set<ConcreteOption> cOptions) {
+		this(cOptions, null);
+	}
+
+	public Set<AbstractOption> getAbstractOptions() {
+		return aOptions;
+	}
+
+	public Set<ConcreteOption> getConcreteOptions() {
+		return cOptions;
+	}
+
+	public boolean containsToken(String token) {
+		return tokens.contains(token);
+	}
+
+	public boolean containsCategory(String categoryName) {
+		return categories.contains(categoryName);
+	}
+
+}
