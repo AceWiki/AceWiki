ab2dddbb1142777cfec97cb58c5fd5fc25e2e017
Tobias Kuhn <kuhntobias@gmail.com>
2013-01-17 10:52:05 +0100

    Add missing libraries; documentation

--- src/ch/uzh/ifi/attempto/acewiki/core/MultilingualSentence.java
+++ src/ch/uzh/ifi/attempto/acewiki/core/MultilingualSentence.java
@@ -2,8 +2,20 @@ package ch.uzh.ifi.attempto.acewiki.core;
 
 import java.util.List;
 
+/**
+ * This class represents a sentence for a multilingual AceWiki engine.
+ * 
+ * @author Kaarel Kaljurand
+ * @author Tobias Kuhn
+ */
 public abstract class MultilingualSentence extends AbstractSentence {
 
+	/**
+	 * Returns a list of sentence details describing translations into all other languages.
+	 * 
+	 * @param currentLanguage The current language (to be excluded).
+	 * @return A list of sentence details on translations.
+	 */
 	public abstract List<SentenceDetail> getTranslations(String currentLanguage);
 
 }
--- src/ch/uzh/ifi/attempto/base/MultiTextContainer.java
+++ src/ch/uzh/ifi/attempto/base/MultiTextContainer.java
@@ -1,3 +1,17 @@
+// This file is part of AceWiki.
+// Copyright 2008-2012, AceWiki developers.
+// 
+// AceWiki is free software: you can redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+// 
+// AceWiki is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along with AceWiki. If
+// not, see http://www.gnu.org/licenses/.
+
 package ch.uzh.ifi.attempto.base;
 
 import java.util.Collection;
@@ -6,10 +20,22 @@ import java.util.List;
 
 import com.google.common.collect.ImmutableList;
 
+/**
+ * This class represents a text that can have multiple variants (all in the same language).
+ * Multiple variants can arise when the internal representation is ambiguous.
+ * 
+ * @author Kaarel Kaljurand
+ * @author Tobias Kuhn
+ */
 public class MultiTextContainer implements Iterable<TextContainer> {
 
 	private final List<TextContainer> mTextContainerList;
 
+	/**
+	 * Generates a new multi-text container.
+	 * 
+	 * @param textContainers The text containers, each representing a variant.
+	 */
 	public MultiTextContainer(TextContainer... textContainers) {
 		mTextContainerList = ImmutableList.copyOf(textContainers);
 		if (mTextContainerList.size() == 0) {
@@ -17,6 +43,11 @@ public class MultiTextContainer implements Iterable<TextContainer> {
 		}
 	}
 
+	/**
+	 * Generates a new multi-text container.
+	 * 
+	 * @param textContainers The text containers, each representing a variant.
+	 */
 	public MultiTextContainer(Collection<TextContainer> textContainers) {
 		mTextContainerList = ImmutableList.copyOf(textContainers);
 		if (mTextContainerList.size() == 0) {
@@ -25,7 +56,7 @@ public class MultiTextContainer implements Iterable<TextContainer> {
 	}
 
 	/**
-	 * Returns the text elements of the first text container.
+	 * Returns the text elements of the first variant.
 	 * 
 	 * @return The text elements.
 	 */
@@ -33,10 +64,21 @@ public class MultiTextContainer implements Iterable<TextContainer> {
 		return get(0).getTextElements();
 	}
 
+	/**
+	 * Returns the text container for the given variant.
+	 * 
+	 * @param index The index of the variant.
+	 * @return The text container for the variant.
+	 */
 	public TextContainer get(int index) {
 		return mTextContainerList.get(index);
 	}
 
+	/**
+	 * Returns the number of variants.
+	 * 
+	 * @return The number of variants.
+	 */
 	public int size() {
 		return mTextContainerList.size();
 	}
