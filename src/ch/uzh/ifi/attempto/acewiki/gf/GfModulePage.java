7296928a17d91679da718e2e3bdf7984ca86de74
Kaarel Kaljurand <kaljurand@gmail.com>
2013-02-01 11:00:00 +0100

    Better labels on the GF module page
    Also fix NPE when entering a GF module page which resulted from
    commit 42dcaa9f44f

--- src/ch/uzh/ifi/attempto/acewiki/gf/GfModulePage.java
+++ src/ch/uzh/ifi/attempto/acewiki/gf/GfModulePage.java
@@ -1,5 +1,7 @@
 package ch.uzh.ifi.attempto.acewiki.gf;
 
+import java.util.List;
+
 import nextapp.echo.app.Column;
 import nextapp.echo.app.Font;
 import nextapp.echo.app.Row;
@@ -14,6 +16,7 @@ import ch.uzh.ifi.attempto.acewiki.core.Comment;
 import ch.uzh.ifi.attempto.acewiki.core.OntologyElement;
 import ch.uzh.ifi.attempto.acewiki.core.Statement;
 import ch.uzh.ifi.attempto.acewiki.gui.ArticlePage;
+import ch.uzh.ifi.attempto.acewiki.gui.CommentEditorHandler;
 import ch.uzh.ifi.attempto.acewiki.gui.StatementMenu;
 import ch.uzh.ifi.attempto.echocomp.MessageWindow;
 import ch.uzh.ifi.attempto.echocomp.SolidLabel;
@@ -23,6 +26,11 @@ import ch.uzh.ifi.attempto.gfservice.GfParseResult;
 import ch.uzh.ifi.attempto.gfservice.GfServiceException;
 import ch.uzh.ifi.attempto.gfservice.GfStorageResult;
 
+/**
+ * TODO: localize strings
+ *
+ * @author Kaarel Kaljurand
+ */
 public class GfModulePage extends ArticlePage {
 
 	private final Logger mLogger = LoggerFactory.getLogger(GfModulePage.class);
@@ -31,6 +39,8 @@ public class GfModulePage extends ArticlePage {
 
 	// TODO: make it a button (not a tab), and enable it only if the grammar is error free
 	private static final String ACTION_MAKE = "Rebuild grammar";
+
+	private static final String acewiki_statementmenu_addgfmodule = "Add GF module content";
 
 	private final OntologyElement mElement;
 	private final GFEngine mEngine;
@@ -60,32 +70,38 @@ public class GfModulePage extends ArticlePage {
 		if (ACTION_MAKE.equals(e.getActionCommand())) {
 			log("page", "pressed: make");
 			integrate();
+		} else if (acewiki_statementmenu_addgfmodule.equals(e.getActionCommand())) {
+			if (!getWiki().isEditable()) {
+				getWiki().showLoginWindow();
+			} else {
+				getWiki().showWindow(CommentEditorHandler.generateCreationWindow(null, this));
+			}
 		}
 	}
 
+
 	protected void doUpdate() {
 		Column textColumn = getTextColumn();
-		StatementMenu dropDown = getDropDown();
 		textColumn.removeAll();
 
-		// Treat comments in the GF Module Page as GF Modules
-		for (Statement s : getArticle().getStatements()) {
-			if (s instanceof Comment) {
-				textColumn.add(new GfModuleComponent((Comment) s, this));
+		// TODO: hack: Treat the first comment in the GF Module Page as a GF module
+		Comment grammarContent = getGrammarContent();
+		if (grammarContent == null) {
+			textColumn.add(new SolidLabel("(grammar module is empty)", Font.ITALIC, 10));
+			if (! getWiki().isReadOnly()) {
+				StatementMenu dropDown = getDropDown();
+				dropDown = new StatementMenu(StatementMenu.EMPTY_TYPE, getWiki(), this);
+				dropDown.addMenuEntry(acewiki_statementmenu_addgfmodule, "acewiki_statementmenu_addgfmoduletooltip");
+				Row addButtonRow = new Row();
+				addButtonRow.add(dropDown);
+				textColumn.add(addButtonRow);
 			}
+		} else {
+			textColumn.add(new GfModuleComponent(grammarContent, this));
 		}
 
-		if (getArticle().getStatements().size() == 0) {
-			textColumn.add(new SolidLabel("(grammar is empty)", Font.ITALIC, 10));
-		}
-
-		if (!getWiki().isReadOnly()) {
-			Row addButtonRow = new Row();
-			addButtonRow.add(dropDown);
-			textColumn.add(addButtonRow);
-		}
 		getTitle().setText(mElement.getWord());
-		parse();
+		parse(); // TODO: we should not parse every time the page is visited
 	}
 
 
@@ -128,12 +144,12 @@ public class GfModulePage extends ArticlePage {
 
 
 	private GfModule getGfModule() {
-		return new GfModule(getName(), getContent());
+		return new GfModule(getName(), getGrammarContent().getText());
 	}
 
 
 	private boolean hasContent() {
-		return ! getArticle().getStatements().isEmpty();
+		return getGrammarContent() != null;
 	}
 
 
@@ -142,8 +158,11 @@ public class GfModulePage extends ArticlePage {
 	}
 
 
-	private String getContent() {
-		return getArticle().getStatements().iterator().next().toString();
+	private Comment getGrammarContent() {
+		List<Statement> statements = getArticle().getStatements();
+		if (statements == null || statements.isEmpty() || ! (statements.get(0) instanceof Comment)) {
+			return null;
+		}
+		return (Comment) statements.get(0);
 	}
-
 }
